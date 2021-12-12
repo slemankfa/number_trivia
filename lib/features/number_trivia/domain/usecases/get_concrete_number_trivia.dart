17b2c7af1638a6935ec60f1132d89bee124c36ff
Sulaiman Farajallah <32030463+slemankfa@users.noreply.github.com>
2021-12-12 14:24:26 +0200

    finished use cases and start working in data Layer

--- lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
+++ lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
@@ -1,16 +1,27 @@
 import 'package:dartz/dartz.dart';
-import 'package:flutter/material.dart';
-import 'package:number_trivia/core/error/failures.dart';
-import 'package:number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:equatable/equatable.dart';
 
+import '../../../../core/error/failures.dart';
+import '../../../../core/usecases/usecase.dart';
+import '../entities/number_trivia.dart';
 import '../repositories/number_trivia_repostiry.dart';
 
-class GetConcreteNumberTrivia {
+class GetConcreteNumberTrivia implements UseCase<NumberTrivia, Params> {
   final NumberTriviaRepostiry repostiry;
 
   GetConcreteNumberTrivia(this.repostiry);
-
-  Future<Either<Failures, NumberTrivia>> excuate({required int number}) async {
-    return await repostiry.getConcreateNumberTrivia(number);
+//  callable classes
+  @override
+  Future<Either<Failures, NumberTrivia>> call(Params params) async {
+    return await repostiry.getConcreateNumberTrivia(params.number);
   }
+}
+
+class Params extends Equatable {
+  final int number;
+
+  const Params({required this.number});
+  @override
+  // TODO: implement props
+  List<Object?> get props => [number];
 }
---
+++ lib/features/number_trivia/domain/usecases/get_random_number_trivia.dart
@@ -0,0 +1,19 @@
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/error/failures.dart';
+import '../../../../core/usecases/usecase.dart';
+import '../entities/number_trivia.dart';
+import '../repositories/number_trivia_repostiry.dart';
+
+class GetRandomNumberTrivia implements UseCase<NumberTrivia, NoParams> {
+  final NumberTriviaRepostiry repostiry;
+
+  GetRandomNumberTrivia(this.repostiry);
+
+  @override
+  Future<Either<Failures, NumberTrivia>> call(params) async {
+    // TODO: implement call
+   return await repostiry.getRandomNumberTrivia();
+  }
+}
+
