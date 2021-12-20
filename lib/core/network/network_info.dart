f861b201b6f46777e5c415a21755a6950efa3313
Sulaiman Farajallah <32030463+slemankfa@users.noreply.github.com>
2021-12-20 13:32:49 +0200

    finished main course

--- lib/core/network/network_info.dart
+++ lib/core/network/network_info.dart
@@ -1,17 +1,18 @@
-import 'package:data_connection_checker/data_connection_checker.dart';
+// import 'package:data_connection_checker/data_connection_checker.dart';
 
 abstract class NetworkInfo {
   Future<bool> get isConnected;
 }
 
 class NetworkInfoImpl implements NetworkInfo {
-  final DataConnectionChecker dataConnectionChecker;
+  // final DataConnectionChecker dataConnectionChecker;
 
-  NetworkInfoImpl(this.dataConnectionChecker);
+  // NetworkInfoImpl(this.dataConnectionChecker);
 
   @override
   // TODO: implement isConnected
-  Future<bool> get isConnected => dataConnectionChecker.hasConnection;
+  Future<bool> get isConnected => Future.value(true);
+  // Future<bool> get isConnected => dataConnectionChecker.hasConnection;
   // Future<bool> get isConnected {
   //   dataConnectionChecker.hasConnection ;
   //   return Future.value(true);
--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,4 +1,4 @@
-import 'package:data_connection_checker/data_connection_checker.dart';
+// import 'package:data_connection_checker/data_connection_checker.dart';
 import 'package:get_it/get_it.dart';
 import 'package:number_trivia/core/network/network_info.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -51,11 +51,11 @@ Future<void> init() async {
   //! Core
   sl.registerLazySingleton(() => InputConverter());
   sl.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(
-        sl(),
+        // sl(),
       ));
   //! Externals
   final sharedPreferences = await SharedPreferences.getInstance();
   sl.registerLazySingleton(() => sharedPreferences);
-  sl.registerLazySingleton(() => http.Client);
-  sl.registerLazySingleton(() => DataConnectionChecker());
+  sl.registerLazySingleton(() => http.Client());
+  // sl.registerLazySingleton(() => DataConnectionChecker());
 }
